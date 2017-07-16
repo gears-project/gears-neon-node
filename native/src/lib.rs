b2d49019b8785c713e70faca8a56a4434a8bcd0c
Michiel Kalkman <michiel@nosuchtype.com>
2017-07-16 21:53:33 +1000

    chore: Set native project name

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -1,13 +1,17 @@
 #[macro_use]
 extern crate neon;
+extern crate xflow;
 
 use neon::vm::{Call, JsResult};
-use neon::js::JsString;
+use neon::js::{JsString, JsBoolean};
+use neon::js::error::{JsError, Kind};
 
 fn hello(call: Call) -> JsResult<JsString> {
     let scope = call.scope;
     Ok(JsString::new(scope, "hello node").unwrap())
 }
+
+
 
 register_module!(m, {
     m.export("hello", hello)
